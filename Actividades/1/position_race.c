a73f3be48ef8d4981e8e70c1204cc140ebb3ba53
Oscar Navarro <onav1411@gmail.com>
2020-09-09 16:09:35 -0600

    Actividad1

--- Actividades/1/position_race.c
+++ Actividades/1/position_race.c
@@ -2,19 +2,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct {
+    size_t total;
+    size_t position;
+    pthread_mutex_t mutex_position;
+} shared_data_t;
+
+typedef struct {
+    size_t thread_num;
+    shared_data_t* shared_data;
+
+} thread_data_t;
+
+
 void* helloWorld(void* args) {
 
-    size_t thread_num = (size_t) args;
+    thread_data_t* data = (thread_data_t*) args;
 
-    printf("Hello world from thread number # %zu\n", thread_num);
+    size_t thread_num = data->thread_num;
+    shared_data_t* shared_data = data->shared_data;
+
+    pthread_mutex_lock(&shared_data->mutex_position);
+    printf("Thread %zu/%zu: I arrived at position %zu\n", thread_num, shared_data->total, shared_data->position);
+    ++shared_data->position;
+    pthread_mutex_unlock(&shared_data->mutex_position);
+    
     return NULL;
 }
 
-
 int main(int argc, char* arg[]) {
-
-    // arg[0] = command
-    // arg[1] = first param
 
     size_t thread_count = 0;
 
@@ -27,17 +43,28 @@ int main(int argc, char* arg[]) {
 
     pthread_t* threads = malloc((size_t)(thread_count * sizeof(pthread_t)));
 
-    for (size_t i = 0; i < thread_count; ++i) {
-        pthread_create(&threads[i], NULL, helloWorld, (void*)i);
-    }
+    shared_data_t* shared_data = (shared_data_t*)calloc(1, sizeof(shared_data_t));
 
-    printf("Hello world from main thread\n");
+    shared_data->total = thread_count;
+    shared_data->position = 1;
+    pthread_mutex_init(&shared_data->mutex_position, /*attr*/ NULL);
+
+    thread_data_t* thread_data_list = malloc((size_t)(thread_count * sizeof(thread_data_t)));
+
+    for (size_t i = 0; i < thread_count; ++i) {
+        thread_data_list[i].thread_num = i;
+        thread_data_list[i].shared_data = shared_data;
+        pthread_create(&threads[i], NULL, helloWorld, (void*)&thread_data_list[i]);
+    }
 
     for (size_t i = 0; i < thread_count; ++i) {
         pthread_join(threads[i], NULL);
     }
 
+    pthread_mutex_destroy(&shared_data->mutex_position);
     free(threads);
+    free(shared_data);
+    free(thread_data_list);
 
     return 0;
 }
