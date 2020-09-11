ef3e9abef389283b2164528ab44b7c875d45bce5
Oscar Navarro <onav1411@gmail.com>
2020-09-11 02:01:27 -0600

    EJercicio2 terminado

--- Laboratorio3/Ej2/hello_ordered_mutex.c
+++ Laboratorio3/Ej2/hello_ordered_mutex.c
@@ -2,42 +2,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+typedef struct {
+    size_t thread_num;
+    pthread_mutex_t mutex_m;
+} mutex_data_t;
+
+
 void* helloWorld(void* args) {
 
-    size_t thread_num = (size_t) args;
-
-    printf("Hello world from thread number # %zu\n", thread_num);
+    mutex_data_t* data = (mutex_data_t*) args;
+    size_t thread_num = data->thread_num;
+    printf("Hello world from thread number # %zu.\n", thread_num);
     return NULL;
 }
 
 
 int main(int argc, char* arg[]) {
 
-    // arg[0] = command
-    // arg[1] = first param
-
     size_t thread_count = 0;
-
     if (argc >= 2) {
         thread_count = (size_t)strtoul(arg[1], NULL, 10);
     } else {
         fprintf(stderr, "Error, invalid number of parameters\n");
         return 1;
     }
-
     pthread_t* threads = malloc((size_t)(thread_count * sizeof(pthread_t)));
+    mutex_data_t* mutex_data_list = malloc((size_t)(thread_count * sizeof(mutex_data_t)));
+
+   
+    for (size_t i = 0; i < thread_count; ++i) {
+    pthread_mutex_init(&mutex_data_list[i].mutex_m, /*attr*/ NULL);
+    
+    } 
+
 
     for (size_t i = 0; i < thread_count; ++i) {
-        pthread_create(&threads[i], NULL, helloWorld, (void*)i);
+        mutex_data_list[i].thread_num = i;
+        pthread_create(&threads[i], NULL, helloWorld, (void*)&mutex_data_list[i]);
+    }
+     
+
+     for (size_t i = 1; i < thread_count; ++i) {
+        pthread_mutex_lock(&mutex_data_list[i].mutex_m);
+    }
+
+    for (size_t i = 0; i < thread_count; ++i) {
+        pthread_join(threads[i], NULL);
+        pthread_mutex_unlock(&mutex_data_list[i+1].mutex_m);
     }
 
     printf("Hello world from main thread\n");
 
-    for (size_t i = 0; i < thread_count; ++i) {
-        pthread_join(threads[i], NULL);
-    }
-
     free(threads);
+    free(mutex_data_list);
+    
 
     return 0;
 }
