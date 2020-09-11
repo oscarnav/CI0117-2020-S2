62ab31890512d7ab4c076de4c2d663c123414838
Oscar Navarro <onav1411@gmail.com>
2020-09-10 20:58:27 -0600

    Ejercicio 1 terminado

--- Laboratorio3/Ej1/thread_creation_time.c
+++ Laboratorio3/Ej1/thread_creation_time.c
@@ -1,12 +1,11 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <math.h>
 
 void* helloWorld(void* args) {
-
-    size_t thread_num = (size_t) args;
-
-    printf("Hello world from thread number # %zu\n", thread_num);
+     
     return NULL;
 }
 
@@ -16,28 +15,36 @@ int main(int argc, char* arg[]) {
     // arg[0] = command
     // arg[1] = first param
 
-    size_t thread_count = 0;
-
+    
+    size_t T = 0;
+    
     if (argc >= 2) {
-        thread_count = (size_t)strtoul(arg[1], NULL, 10);
+
+       T = (size_t)strtoul(arg[1], NULL, 10);
     } else {
-        fprintf(stderr, "Error, invalid number of parameters\n");
-        return 1;
+        T = (size_t)1;
     }
+    
 
-    pthread_t* threads = malloc((size_t)(thread_count * sizeof(pthread_t)));
+    long nanos_menor = 0;
 
-    for (size_t i = 0; i < thread_count; ++i) {
-        pthread_create(&threads[i], NULL, helloWorld, (void*)i);
+    for (size_t i = 0; i < T; ++i) {
+        struct timespec before;
+        clock_gettime(CLOCK_REALTIME, &before);
+
+        pthread_t thread;
+        pthread_create(&thread, NULL, helloWorld, NULL);
+        pthread_join(thread, NULL);
+        
+        struct timespec after;
+        clock_gettime(CLOCK_REALTIME, &after);
+        if(i==0 || nanos_menor > after.tv_nsec-before.tv_nsec){
+        nanos_menor = after.tv_nsec-before.tv_nsec;
+        }
+        
     }
-
-    printf("Hello world from main thread\n");
-
-    for (size_t i = 0; i < thread_count; ++i) {
-        pthread_join(threads[i], NULL);
-    }
-
-    free(threads);
-
+     
+     //float conv = 1e9;
+    printf("Minimum thread creation and destruction time was: %fs, among %ld trials\n", nanos_menor/1e9, T ); 
     return 0;
 }
