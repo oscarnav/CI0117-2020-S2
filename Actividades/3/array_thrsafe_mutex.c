a835f1b1df1010986e29e911edbf6fcce95d21b7
oscarnav <65473298+oscarnav@users.noreply.github.com>
2020-09-18 08:16:07 -0600

    Create array_thrsafe_mutex.c

---
+++ Actividades/3/array_thrsafe_mutex.c
@@ -0,0 +1,7 @@
+#define _XOPEN_SOURCE 600 // Optional for VS Studio to recognize certain values
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
