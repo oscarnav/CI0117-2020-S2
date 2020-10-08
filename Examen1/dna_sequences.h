08ae26ef20e4613b303abc1cf2da1c00e3892fee
oscarnav <65473298+oscarnav@users.noreply.github.com>
2020-10-08 13:55:02 -0600

    Add files via upload

---
+++ Examen1/dna_sequences.h
@@ -0,0 +1,16 @@
+#ifndef DNA_H
+#define DNA_H
+
+#include <pthread.h>
+
+
+static const char DNA1[] = {l,d,s,g,w,d,f,o}:
+static const char DNA1[] = {a,b,c,d,e,f,g,h}:
+static const int DNA1_len = 8; 
+static const int DNA2_len = 8; 
+
+void* ocurSecuencia1(void* args);
+void* ocurSecuencia2(void* args);
+int main(int argc, char* arg[]);
+
+#endif
