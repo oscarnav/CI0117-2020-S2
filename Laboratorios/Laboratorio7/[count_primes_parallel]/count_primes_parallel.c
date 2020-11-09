23eda205c0611a95de2a8eea198733cd1090691d
Oscar Navarro <onav1411@gmail.com>
2020-11-09 13:37:27 -0600

    lab7

---
+++ Laboratorios/Laboratorio7/[count_primes_parallel]/count_primes_parallel.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+#include <math.h>
+
+
+int esPrimo(size_t number){
+	
+	if(number < 2){
+		return 0;
+	}
+	if(number == 2){
+		return 1;
+	}
+	if(number % 2 == 0){
+		return 0;
+	}
+	
+	for(size_t i = 3, last = (size_t)(double)sqrt(number); i <= last; i += 2){
+		if(number%i == 0){
+			return 0;
+		}
+	}
+	return 1;
+}	
+
+
+int main(int argc, char* arg[]) {
+size_t ultimo = 0;
+
+size_t contador = 0;
+	if (argc >= 2) {
+		ultimo = (size_t)strtoul(arg[1], NULL, 10);
+	
+		
+    } else {
+        fprintf(stderr, "Error, invalid number of parameters\n");
+        return 1;
+    }
+
+	#pragma omp parallel for reduction(+: contador)
+	for (size_t i = 2; i <= ultimo; i++) {        
+		 if(esPrimo(i)){
+			
+			 ++contador;
+		 }
+
+    }
+	printf("numero de primos: %zu\n", contador);
+	
+	
+	
+	
+}
+
+
+
